603b95e9e6830af4aa5bd3fb0f76582d2e01d315
Moumita <moumitaKanrar526@gmail.com>
2024-02-22 20:55:03 +0530

    127: create save post feature Entity class

---
+++ src/main/java/com/myblog/myblog13/entity/Post.java
@@ -0,0 +1,24 @@
+package com.myblog.myblog13.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="posts")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+
+    private String title;
+
+    private String description;
+    private  String content;
+
+}
